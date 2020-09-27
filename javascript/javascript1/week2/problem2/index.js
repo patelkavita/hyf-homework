456efff584c9ae6a394f3417986f37f0e9f64751
Kavita Patel <kavita7013@gmail.com>
2020-09-27 20:06:42 +0530

    solved problem 2 of week 2

--- javascript/javascript1/week2/problem2/index.js
+++ javascript/javascript1/week2/problem2/index.js
@@ -16,50 +16,40 @@ console.log(getEventWeekday(2)); // Logs out "Sunday"
 You should get the today's day from the system.
 
 Hint: use remainder operator, array indexes and investigate new Date in js.*/
-'use strict';
-let dayValue, eventday,day1,event1;
+"use strict";
+let dayValue, eventday, day1, event1;
 
-function getEventWeekday() {
+const daysInAWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-        function temp1(days) {
-                
-                if (days == "sunday") {
-                  return (dayValue = 0);
-                } else if (days == "tuesday") {
-                  return (dayValue = 2);
-                } else if (days == "wednesday") {
-                  return (dayValue = 3);
-                } else if (days == "thursday") {
-                  return (dayValue = 4);
-                } else if (days == "friday") {
-                  return (dayValue = 5);
-                } else if (days == "saturday") {
-                  return (dayValue = 6);
-                } else if (days == "monday") {
-                  return (dayValue = 1);
-                } else {
-                  return " please enter proper day";
-                }
-            
-              }
-   day1 = document.getElementById("day").value;  //user input
-  event1=document.getElementById("event").value;  //user input
-  
-function eventValue(event1){  
-        return event1%7;
+function findWeekdayIndex(dayName) {
+  dayName = dayName.trim().toLocaleLowerCase();
+
+  return daysInAWeek.findIndex(
+    (weekDay) => weekDay.toLocaleLowerCase().trim() === dayName
+  );
 }
+function getEventWeekday() {
+  day1 = document.getElementById("day").value;
+  event1 = document.getElementById("event").value;
 
-  eventday= temp1(day1)+eventValue(event1);  //returns numeric value of both functions
-  const x= eventday%7;
-switch(x){
-        
-        case 1: console.log("Event happend on Monday"); break;
-        case 2: console.log(" Event Happend on Tuesday"); break;
-        case 3: console.log(" Event Happend on Wednesday"); break;
-        case 4: console.log(" Event Happend on Thursday"); break;
-        case 5: console.log(" Event Happend on Friday"); break;
-        case 6: console.log(" Event Happend on Saturday"); break;
-        case 0: console.log(" Event Happend on Sunday"); break;
-        default: console.log(" please enter proper value");
-}  
+  function eventValue(event1) {
+    return event1 % 7;
+  }
+
+  const indexForGivenDay = findWeekdayIndex(day1);
+  if (indexForGivenDay === -1) {
+    return;
+  }
+
+  eventday = findWeekdayIndex(day1) + eventValue(event1);
+  const resultDay = daysInAWeek[eventday % 7];
+  console.log("Event will be on "+resultDay);
 }
